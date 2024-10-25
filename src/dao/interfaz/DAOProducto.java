462ade69bf2ea0f359c8115d9800819da226db73
Emmanuel Rivera <U22222056@utp.edu.pe>
2024-10-25 11:26:32 -0500

    Eliminación de contenido innecesario.

--- src/dao/interfaz/DAOProducto.java
+++ src/dao/interfaz/DAOProducto.java
@@ -4,9 +4,15 @@ import dto.DTOProducto;
 import java.util.List;
 
 public interface DAOProducto {
+
     public void agregarProducto(DTOProducto producto);
+    
     public void actualizarProducto(DTOProducto producto);
+    
     public void eliminarProducto(long idProducto);
+    
     public DTOProducto obtenerProductoPorId(long idProducto);
+    
     public List<DTOProducto> obtenerTodosLosProductos();
+
 }
