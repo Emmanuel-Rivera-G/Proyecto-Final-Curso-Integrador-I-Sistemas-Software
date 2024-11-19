abcaf855965132c78ae5e0650e7ee26686f5fb4c
YinyerAle05 <u22218751@utp.edu.pe>
2024-11-19 11:37:35 -0500

    Creación de la implementación de DAOSalida para la conexión a la BD. - Falta implementar métodos

---
+++ src/dao/implemetacion/DAOSalidaImpl.java
@@ -0,0 +1,38 @@
+package dao.implemetacion;
+
+import dao.interfaz.DAOSalida;
+import dto.DTOSalida;
+import java.util.List;
+
+/**
+ *
+ * @author Yinyer
+ */
+public class DAOSalidaImpl implements DAOSalida {
+
+    @Override
+    public List<DTOSalida> obtenerTodasLasSalidas() {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    public DTOSalida obtenerSalidaPorId(int idSalida) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    public int guardarSalida(DTOSalida dtoSalida) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    public boolean actualizarSalida(DTOSalida dtoSalida) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    public boolean eliminarSalida(int idSalida) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+    
+}
