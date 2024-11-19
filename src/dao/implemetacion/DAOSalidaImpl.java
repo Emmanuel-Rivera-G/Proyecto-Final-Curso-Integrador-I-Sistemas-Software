4beec40a94e66440f6e78c4fcb67b7dbd1f7507a
YinyerAle05 <u22218751@utp.edu.pe>
2024-11-19 11:41:59 -0500

    Implementación del método de DAOSalidaImpl obtenerTodasLasSalidas

--- src/dao/implemetacion/DAOSalidaImpl.java
+++ src/dao/implemetacion/DAOSalidaImpl.java
@@ -1,7 +1,12 @@
 package dao.implemetacion;
 
+import java.sql.Connection;
+import java.sql.Statement;
+import java.sql.ResultSet;
 import dao.interfaz.DAOSalida;
 import dto.DTOSalida;
+import java.sql.SQLException;
+import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -9,10 +14,26 @@ import java.util.List;
  * @author Yinyer
  */
 public class DAOSalidaImpl implements DAOSalida {
+    
+    private Connection connection;
+
+    public DAOSalidaImpl(Connection connection) {
+        this.connection = connection;
+    }
 
     @Override
     public List<DTOSalida> obtenerTodasLasSalidas() {
-        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+        List<DTOSalida> salidas = new ArrayList<>();
+        String sql = "SELECT * FROM Salida";
+        try (Statement stmt = connection.createStatement();
+             ResultSet rs = stmt.executeQuery(sql)) {
+            while (rs.next()) {
+                salidas.add(mapResultSetToSalida(rs));
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return salidas;
     }
 
     @Override
